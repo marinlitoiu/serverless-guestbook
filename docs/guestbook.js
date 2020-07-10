7528ca0b265a1e7a6f205bc2b2acfaf80c79f21f
Marin Litoiu <mlitoiu@yorku.ca>
2020-07-09 20:48:47 -0400

    Update guestbook.js
    updated url

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://ed0ae8d6.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
